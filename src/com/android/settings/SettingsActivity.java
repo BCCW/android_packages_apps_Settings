00d50cdf2bfb5ee65d13aef398509858c72d1911
espes <espes@pequalsnp.com>
2016-12-12 19:57:25 -0800

    patches for NEOS

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -733,6 +733,16 @@ public class SettingsActivity extends Activity
         mHomeActivitiesCount = getHomeActivitiesCount();
     }
 
+    // comma hax
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return false;
+    }
+
     private int getHomeActivitiesCount() {
         final ArrayList<ResolveInfo> homeApps = new ArrayList<ResolveInfo>();
         getPackageManager().getHomeActivities(homeApps);
@@ -1252,7 +1262,7 @@ public class SettingsActivity extends Activity
     private void updateTilesList(List<DashboardCategory> target) {
         final boolean showDev = mDevelopmentPreferences.getBoolean(
                 DevelopmentSettings.PREF_SHOW,
-                android.os.Build.TYPE.equals("eng"));
+                true/*android.os.Build.TYPE.equals("eng")*/);
 
         final UserManager um = (UserManager) getSystemService(Context.USER_SERVICE);
 
